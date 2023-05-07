063b962c1f21e8d83337e4a9e9b2bb2362c6b7ed
alex <alexandrkorkhin@yandex.ru>
2023-05-07 14:22:25 +0300

    какиш

--- app/src/main/java/com/example/university/programs.java
+++ app/src/main/java/com/example/university/programs.java
@@ -1,6 +1,5 @@
 package com.example.university;
 
-
 import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
@@ -8,22 +7,16 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.List;
-
+import java.util.ArrayList;
 
 public class programs extends AppCompatActivity {
 
@@ -37,22 +30,22 @@ public class programs extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Итмо");
         mProgramsList = (ListView) findViewById(R.id.programs_list);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_program);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_program, new ArrayList<String>());
         mProgramsList.setAdapter(adapter);
 
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 adapter.clear();
                 for (DataSnapshot programSnapshot : dataSnapshot.getChildren()) {
                     String programName = programSnapshot.getValue(String.class);
                     adapter.add(programName);
                 }
+                adapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
                 Log.w(TAG, "loadPrograms:onCancelled", databaseError.toException());
             }
         });
